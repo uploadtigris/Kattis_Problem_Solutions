a3de07787e8aea88ba7df4598764b9018269f265
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2023-09-22 11:45:32 -0500

    Update Pot.py

--- Pot.py
+++ Pot.py
@@ -1,1 +1,32 @@
+import sys
+
+#output number
+x = 0
+
+#read first line
+a = sys.stdin.readline()
+a = int(a.strip())
+
+#read new line and assign it to a variable
+elems = []
+i = 0
+
+while i < a:
+  y = sys.stdin.readline()
+  y = y.strip() #keep a string for parsing
+
+  # parse the number from the exponent
+  object = [[y[:-1],y[-1]]] 
+  elems += object
+  i+=1
+
+#convert to integers
+new_elems = [list(map(int, i)) for i in elems]
+
+# assign x the values of the sum of the nums raised to their exp's
+for item in new_elems:
+  x += item[0]**item[1]
+
+# output x
+print(x)
 
