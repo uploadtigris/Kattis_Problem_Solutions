5c2da1ac8a0d05eefdcaa16dd84751aaa5448a3d
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2023-09-22 10:58:24 -0500

    Update Planina.py

--- Planina.py
+++ Planina.py
@@ -1,1 +1,19 @@
 
+import sys
+import math
+
+# create variables
+num_points = 2
+
+N = int(input())
+
+
+#create a for loop
+i = 0
+while i <= N-1:
+    y = (num_points * 2 - 1)**2
+    y = int(y)
+    num_points = math.sqrt(y)
+    i+=1
+    print(y)
+
